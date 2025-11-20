19d2b230cff28730fbb01bdd071d49071b90b8d1
Ericka Tuchan <125705781+ErickaT23@users.noreply.github.com>
2025-11-20 10:09:41 -0600

    invitado

--- invitados.js
+++ invitados.js
@@ -1,7 +1,4 @@
 // invitados.js
 window.invitados = [
-  { id: 1, nombre: "Familia García", pases: 4 },
-  { id: 2, nombre: "María López", pases: 2 },
-  { id: 3, nombre: "Carlos y Ana", pases: 3 },
-  { id: 4, nombre: "José Martínez", pases: 1 },
+  { id: 1, nombre: "Familia Bätzel Mejía", pases: 3 },
 ];
