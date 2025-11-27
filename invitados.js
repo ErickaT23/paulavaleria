4ba96c2832280912980c83a48dbcf389c195a245
Ericka Tuchan <125705781+ErickaT23@users.noreply.github.com>
2025-11-26 21:15:48 -0600

    96-100

--- invitados.js
+++ invitados.js
@@ -94,6 +94,11 @@ window.invitados = [
   { id: 92, nombre: "Señora Bárbara Arriol", pases: 2 },
   { id: 93, nombre: "Señora Lizza Dávila ", pases: 2 },
   { id: 94, nombre: "Señor Arnulfo López y esposa", pases: 2 },
-  { id: 95, nombre: "Señora Mireya Mazariegos", pases: 2 }
+  { id: 95, nombre: "Señora Mireya Mazariegos", pases: 2 },
+  { id: 96, nombre: "Ingeniera Marcela Peláez Pérez", pases: 2 },
+  { id: 97, nombre: "Licenciado Jorge Guillermo Aráuz Aguilar", pases: 2 },
+  { id: 98, nombre: "Ingeniera Georgina Peláez Petz", pases: 2 },
+  { id: 99, nombre: "Señor Gersson Mejía y esposa" , pases: 2 },
+  { id: 100, nombre: "Señor Luis Mejía y esposa", pases: 2 }
 ];
 
